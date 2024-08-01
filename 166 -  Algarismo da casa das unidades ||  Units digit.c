d48b3a36fa42086978b8204cc96e22abd0e40fcd
hollyver-hub <bruno.martires@arapiraca.ufal.br>
2024-08-01 14:02:23 -0300

    Create 166 -  Algarismo da casa das unidades ||  Units digit.c

---
+++ 166 -  Algarismo da casa das unidades ||  Units digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    
+    scanf("%d", &num);
+    printf("%d", num%10);
+    
+	return 0;
+}
